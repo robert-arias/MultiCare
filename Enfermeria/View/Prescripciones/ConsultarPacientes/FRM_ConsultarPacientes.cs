7d986cbcc6eb6c4c56caa45c1d4f15cba2d0ad49
robert-arias <robertariascastro@outlook.com>
2020-03-14 12:44:36 -0600

    Prescripción -consultar pacientes UI

---
+++ Enfermeria/View/Prescripciones/ConsultarPacientes/FRM_ConsultarPacientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enfermeria.View.Prescripciones.ConsultarPacientes {
+    public partial class FRM_ConsultarPacientes : Form {
+        public FRM_ConsultarPacientes() {
+            InitializeComponent();
+        }
+    }
+}
